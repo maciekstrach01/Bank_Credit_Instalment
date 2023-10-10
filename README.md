c670b96853c2296280cb620bf807eba6544d7258
Maciej Strach <maciej.strach01@gmail.com>
2023-10-10 18:53:02 +0200

    Add

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Bank Credit Instalment 
+
+This code is a simple loan instalment calculator. It runs in the console and allows the user to enter the loan amount and the number of instalments, then calculates the monthly loan instalment and displays the result.
+
+## 🛠 Skills
+C#
+
+
+## Screenshots
+
+
+![credit instalment](https://github.com/maciekstrach01/Library_project_cpp/assets/146733279/f79b6bab-4bbe-455c-afa3-10dca626b04f)
+
+
+
+## Running Tests
+
+To run the tests, you must have Visual Studio or another preferred IDE for C# installed. Configure the IDE for C#, translate the code into machine language i.e. compile it, and then run the program.
+
+```
+
---
+++ main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace InstalmentCreditCalculator
+{
+    class Program
+    {
+        static double GetValidDoubleInput(string prompt, double minValue, double maxValue)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    string userInput = Console.ReadLine();
+                    double result = double.Parse(userInput);
+
+                    if (result < minValue || result > maxValue)
+                        throw new ArgumentOutOfRangeException();
+
+                    return result;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine($"Input out of range. Please enter a number between {minValue} and {maxValue}.");
+                }
+            }
+        }
+
+        static int GetValidIntInput(string prompt, int minValue, int maxValue)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    string userInput = Console.ReadLine();
+                    int result = int.Parse(userInput);
+
+                    if (result < minValue || result > maxValue)
+                        throw new ArgumentOutOfRangeException();
+
+                    return result;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine($"Input out of range. Please enter an integer between {minValue} and {maxValue}.");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double minLoanAmount = 10000;
+            double maxLoanAmount = 100000;
+            int minNumberOfRates = 6;
+            int maxNumberOfRates = 96;
+
+            double loanamount = GetValidDoubleInput($"Specify the loan amount ({minLoanAmount} to {maxLoanAmount} PLN): ", minLoanAmount, maxLoanAmount);
+            int numberofrates = GetValidIntInput($"Enter the number of instalments ({minNumberOfRates} to {maxNumberOfRates}): ", minNumberOfRates, maxNumberOfRates);
+
+            double rate = 0.05;
+            double interests = loanamount * rate;
+            double instalmentamount = (loanamount + interests) / numberofrates;
+
+            Console.WriteLine($"The monthly loan instalment is: {instalmentamount:N2} PLN.");
+        }
+    }
+}
+
